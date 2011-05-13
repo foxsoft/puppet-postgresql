954a17768e6f3ac2240578ed06c92e07bee076df
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-13 19:11:24 +0100

    Ensure development libraries are installed - move pg90 specific stuff into 90 include

--- manifests/classes/postgresql-redhat-v9-0.pp
+++ manifests/classes/postgresql-redhat-v9-0.pp
@@ -1,3 +1,8 @@
 class postgresql::redhat::v9-0 inherits postgresql::redhat::base {
+  exec { "install-pgdg":
+      command => "/bin/rpm -i http://yum.pgrpms.org/reporpms/9.0/pgdg-centos-9.0-2.noarch.rpm",
+      unless => "/usr/bin/yum list installed | /bin/grep pgdg"
+  }
   package { "postgresql90-server": ensure => installed }
+  package { "postgresql-devel": ensure => installed }
 }
