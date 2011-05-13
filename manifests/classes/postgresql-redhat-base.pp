0671c25f5009467672fb06ff5b91dce5a300585f
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-13 16:31:11 +0100

    Whoops, this should be redhat

--- manifests/classes/postgresql-redhat-base.pp
+++ manifests/classes/postgresql-redhat-base.pp
@@ -1,4 +1,4 @@
-class postgresql::debian::base inherits postgresql::base {
+class postgresql::redhat::base inherits postgresql::base {
   yumrepo { "PGDG":
      baseurl => "http://yum.pgrpms.org/reporpms/9.0/pgdg-centos-9.0-2.noarch.rpm",
      descr => "PostgreSQL PGDG RPMs for CentOS ",
