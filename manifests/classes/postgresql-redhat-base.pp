6a2d612a82d8f32ba308973ba99c9f1f109c5c2c
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-13 17:21:29 +0100

    Use absolute paths

--- manifests/classes/postgresql-redhat-base.pp
+++ manifests/classes/postgresql-redhat-base.pp
@@ -1,6 +1,6 @@
 class postgresql::redhat::base {
   exec { "install-pgdg":
-      command => "rpm -i http://yum.pgrpms.org/reporpms/9.0/pgdg-centos-9.0-2.noarch.rpm",
-      unless => "yum list installed | grep pgdg"
+      command => "/bin/rpm -i http://yum.pgrpms.org/reporpms/9.0/pgdg-centos-9.0-2.noarch.rpm",
+      unless => "/usr/bin/yum list installed | /bin/grep pgdg"
   }
 }
