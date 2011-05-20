1efbb4802099dae185990d0cbe4bd72e27dcd4c3
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-20 11:02:35 +0100

    Make sure pgdg is installed

--- manifests/classes/postgresql-redhat-v9-0.pp
+++ manifests/classes/postgresql-redhat-v9-0.pp
@@ -3,6 +3,6 @@ class postgresql::redhat::v9-0 inherits postgresql::redhat::base {
       command => "/bin/rpm -i http://yum.pgrpms.org/reporpms/9.0/pgdg-centos-9.0-2.noarch.rpm",
       unless => "/usr/bin/yum list installed | /bin/grep pgdg"
   }
-  package { "postgresql90-server": ensure => installed }
-  package { "postgresql90-devel": ensure => installed }
+  package { "postgresql90-server": ensure => installed, require => Exec["install-pgdg"] }
+  package { "postgresql90-devel": ensure => installed, require => Exec["install-pgdg"] }
 }
