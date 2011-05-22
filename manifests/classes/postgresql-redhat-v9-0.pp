114572acb913c8ba9fba577f1d238d8ac18ba307
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-22 21:23:40 +0100

    Add an init db prerequisite

--- manifests/classes/postgresql-redhat-v9-0.pp
+++ manifests/classes/postgresql-redhat-v9-0.pp
@@ -6,15 +6,22 @@ class postgresql::redhat::v9-0 inherits postgresql::redhat::base {
   package { "postgresql90-server": ensure => installed, require => Exec["install-pgdg"] }
   package { "postgresql90-devel": ensure => installed, require => Exec["install-pgdg"] }
   
+  exec { "init-db":
+    command => "service postgresql-9.0 start",
+    require => Package["postgresql90-server"],
+  }
+  
+  
   Service["postgresql-9.0"] {
     start   => "service postgresql-9.0 start",
     status  => "service postgresql-9.0 status",
     stop    => "service postgresql-9.0 stop",
     restart => "service postgresql-9.0 restart",
-    require => Package["postgresql90-server"],
+    require => Exec["init-db"],
   }
   
   service { "postgresql-9.0":
     ensure => running,
   }
+  
 }
