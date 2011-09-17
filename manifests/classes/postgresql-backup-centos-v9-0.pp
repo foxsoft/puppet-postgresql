4f689f2cd77f426cf06f6de00af8201d11daace1
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-09-18 00:19:01 +0100

    Dont backup unless pgserver is there

--- manifests/classes/postgresql-backup-centos-v9-0.pp
+++ manifests/classes/postgresql-backup-centos-v9-0.pp
@@ -1,36 +1,39 @@
 class postgresql::backup::centos::v9-0 {
-
-  $postgresql_backupdir = "/var/backups/pgsql"
   
-  file {"/var/backups":
-    ensure => directory,
-    owner => "root",
-    group => "root",
-    mode => 755
-  }
+  if !defined (Package["postgresql-server"]) {
   
-  file {$postgresql_backupdir:
-    ensure  => directory,
-    owner   => "postgres",
-    group   => "postgres",
-    mode    => 755,
-    require => [Package["postgresql90-server"], User["postgres"]],
-  }
+    $postgresql_backupdir = "/var/backups/pgsql"
+  
+    file {"/var/backups":
+      ensure => directory,
+      owner => "root",
+      group => "root",
+      mode => 755
+    }
+  
+    file {$postgresql_backupdir:
+      ensure  => directory,
+      owner   => "postgres",
+      group   => "postgres",
+      mode    => 755,
+      require => [Package["postgresql90-server"], User["postgres"]],
+    }
 
-  file { "/usr/local/bin/pgsql-backup.sh":
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 755,
-    content => template("postgresql/pgsql-backup.sh.erb"),
-    require => File[$postgresql_backupdir],
-  }
+    file { "/usr/local/bin/pgsql-backup.sh":
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => 755,
+      content => template("postgresql/pgsql-backup.sh.erb"),
+      require => File[$postgresql_backupdir],
+    }
 
-  cron { "pgsql-backup":
-    command => "/usr/local/bin/pgsql-backup.sh",
-    user    => "postgres",
-    hour    => 2,
-    minute  => 0,
-    require => [User["postgres"], File["/usr/local/bin/pgsql-backup.sh"]],
+    cron { "pgsql-backup":
+      command => "/usr/local/bin/pgsql-backup.sh",
+      user    => "postgres",
+      hour    => 2,
+      minute  => 0,
+      require => [User["postgres"], File["/usr/local/bin/pgsql-backup.sh"]],
+    }
   }
 }
