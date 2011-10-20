ed11bce498598306317c5a5191f9b7791acaa1f5
Omar Qureshi <omar@omarqureshi.net>
2011-10-20 10:55:08 +0100

    Use yumrepo instead - ignore gpgcheck

--- manifests/classes/postgresql-centos-v9-0.pp
+++ manifests/classes/postgresql-centos-v9-0.pp
@@ -1,10 +1,21 @@
 class postgresql::centos::v9-0 inherits postgresql::centos::base {
-  exec { "install-pgdg":
-      command => "/bin/rpm -i http://yum.pgrpms.org/reporpms/9.0/pgdg-centos-9.0-2.noarch.rpm",
-      creates => "/etc/yum.repos.d/pgdg-90-centos.repo"
+  yumrepo { "pgdg90":
+    baseurl => "http://yum.postgresql.org/9.0/redhat/rhel-$releasever-$basearch",
+    descr => "PostgreSQL 9.0 $releasever - $basearch",
+    enabled => 1,
+    gpgcheck => 0,
   }
-  package { "postgresql90-server": ensure => installed, require => Exec["install-pgdg"] }
-  package { "postgresql90-devel": ensure => installed, require => Exec["install-pgdg"] }
+
+  yumrepo { "pgdg90-source":
+    baseurl => "http://yum.postgresql.org/srpms/9.0/redhat/rhel-$releasever-$basearch",
+    descr => "PostgreSQL 9.0 $releasever - $basearch - source",
+    enabled => 0,
+    gpgcheck => 0,
+    failovermethod => "priority",
+  }
+
+  package { "postgresql90-server": ensure => installed, require => [Yumrepo["pgdg90"], Yumrepo["pgdg90-source"]] }
+  package { "postgresql90-devel": ensure => installed, require => [Yumrepo["pgdg90"], Yumrepo["pgdg90-source"]] }
 
   exec { "create-user":
     command => "/bin/su postgres -c '/usr/pgsql-9.0/bin/createuser --superuser deploy' || true",
